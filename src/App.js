62ed82e3d72389ce747bfc561f9d127ef2288abd
fullstacknaga <fullstacknaga@gmail.com>
2022-05-29 23:56:31 +0530

    some comments for reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import './styles.css';
 
 function App() {
+
+
+
+  /*
+
+  go to this link in youtube for this calculator app
+
+  https://www.youtube.com/watch?v=DgRrrOt0Vr8
+
+
+   
+  We are using useReducer hook.
+  It is similar to the useState hook
+
+  go to this link for ReducerHook 
+  https://www.youtube.com/watch?v=kK_Wqx3RnHk
+
+
+  to learn CSS grid
+  https://www.youtube.com/watch?v=9zBsdzdE4sM
+
+  to learn css Box Model
+  https://www.youtube.com/watch?v=rIO5326FgPE
+
+  
+
+
+
+
+
+  It allows for custom state logic
+  If you find yourself keeping track of multiple pieces of state that relay on complex logic, useReduce may be useful
+
+  useReducer takes two arguments useReducer(<reducer>,<Initial State>)
+  The reducer function contains your custom state logic and the initialState can be a simple value but generally will contain a object.
+  
+  did't got it look at below link
+
+  https://www.w3schools.com/react/react_usereducer.asp
+
+
+
+
+   */
   return (
     <div className="calculator-grid">
      <div className='output'>
@@ -8,7 +52,7 @@ function App() {
        <div className='current-operand'>5,000</div>
      </div>
 
-     
+
       <button className='span-two'>AC</button>
       <button>DEL</button>
       <button>/</button>
